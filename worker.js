8342f580952c58eb908863d858b167157f20ed39
Quazi <q.hossain@ymail.com>
2017-11-04 04:20:43 -0400

    institution and getOptionsFromDropdown functions work

--- worker.js
+++ worker.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const HTML = require('html-parse-stringify');
 
 let options = {
     url: '',
@@ -6,10 +7,30 @@ let options = {
     jar: request.jar()
 };
 
+let getOptionsFromDropdown = (body, id, callback) => {
+    let startingIndex = body.indexOf(id);
+    let selectHtml = body.substring(startingIndex, body.indexOf("</select>"));
+    let ast = HTML.parse(selectHtml);
+
+    let values = {};
+    for(let i = 2; i < ast[0].children.length; i++){
+        if(ast[0].children[i].type === 'text')
+            continue;
+        values[ast[0].children[i].children[0].content] = ast[0].children[i].attrs.value;
+    }
+    callback(values);
+};
+
 let institutions = (callback) => {
     request.get(options, function(error, response, body){
         if(error){
             console.log('CUNYfirst is currently offline.');
         }
+        let id = '<select name=\'CLASS_SRCH_WRK2_INSTITUTION$31$\'';
+        getOptionsFromDropdown(body, id, callback);
     })
 };
+
+institutions(function(r){
+    console.log(r);
+});
