2ce75500d407379543f57982ca4c381f67bf1986
Quazi <q.hossain@ymail.com>
2017-11-05 20:02:18 -0500

    can search by class number and search specific courses now

--- worker.js
+++ worker.js
@@ -32,7 +32,7 @@ let getICValues = (body) => {
     return ICValues;
 };
 
-let urlProducer = (ICStateNum, ICSID, inst, session) => {
+let urlProducer = (ICStateNum, ICSID, inst, term) => {
     return 'https://hrsa.cunyfirst.cuny.edu/psc/cnyhcprd/GUEST/HRMS/c/COMMUNITY_ACCESS.CLASS_SEARCH.GBL?ICAJAX=1&ICNAVTYPEDROPDOWN=0&ICType=Panel&ICElementNum=0&ICStateNum='
         +ICStateNum+
         '&ICAction=CLASS_SRCH_WRK2_STRM$35$&ICXPos=0&ICYPos=0&ResponsetoDiffFrame=-1&TargetFrameName=None&FacetPath=None&ICFocus&ICSaveWarningFilter=0&ICChanged=-1&ICAutoSave=0&ICResubmit=0&ICSID='
@@ -40,22 +40,47 @@ let urlProducer = (ICStateNum, ICSID, inst, session) => {
         '=&ICActionPrompt=false&ICBcDomData=undefined&ICFind&ICAddCount&ICAPPCLSDATA&CLASS_SRCH_WRK2_INSTITUTION$31$='
         +inst+
         '&CLASS_SRCH_WRK2_STRM$35$='
-        +session
+        +term
 };
 
-let urlProducerClasses = (ICStateNum, ICSID, session, subject) => {
+let urlProducerAllClasses = (ICStateNum, ICSID, term, subject) => {
     return 'https://hrsa.cunyfirst.cuny.edu/psc/cnyhcprd/GUEST/HRMS/c/COMMUNITY_ACCESS.CLASS_SEARCH.GBL?ICAJAX=1&ICNAVTYPEDROPDOWN=0&ICType=Panel&ICElementNum=0&ICStateNum='
         +ICStateNum+
         '&ICAction=CLASS_SRCH_WRK2_SSR_PB_CLASS_SRCH&ICXPos=0&ICYPos=0&ResponsetoDiffFrame=-1&TargetFrameName=None&FacetPath=None&ICFocus&ICSaveWarningFilter=0&ICChanged=-1&ICAutoSave=0&ICResubmit=0&ICSID='
         +ICSID+
         '=&ICActionPrompt=false&ICBcDomData=undefined&ICFind&ICAddCount&ICAPPCLSDATA&CLASS_SRCH_WRK2_STRM$35$='
-        +session+
+        +term+
         '&SSR_CLSRCH_WRK_SUBJECT_SRCH$0='
         +subject+
         '&SSR_CLSRCH_WRK_SSR_EXACT_MATCH1$1=G&SSR_CLSRCH_WRK_CATALOG_NBR$1=0&SSR_CLSRCH_WRK_SSR_OPEN_ONLY$chk$5=N'
 };
 
-let section = (inst, term, subject, callback) => {
+let urlProducerSpecificCourse = (ICStateNum, ICSID, term, subject, course) => {
+    return 'https://hrsa.cunyfirst.cuny.edu/psc/cnyhcprd/GUEST/HRMS/c/COMMUNITY_ACCESS.CLASS_SEARCH.GBL?ICAJAX=1&ICNAVTYPEDROPDOWN=0&ICType=Panel&ICElementNum=0&ICStateNum='
+        +ICStateNum+
+        '&ICAction=CLASS_SRCH_WRK2_SSR_PB_CLASS_SRCH&ICXPos=0&ICYPos=98&ResponsetoDiffFrame=-1&TargetFrameName=None&FacetPath=None&ICFocus=&ICSaveWarningFilter=0&ICChanged=-1&ICAutoSave=0&ICResubmit=0&ICSID='
+        +ICSID+
+        '=&ICActionPrompt=false&ICBcDomData=undefined&ICFind=&ICAddCount=&ICAPPCLSDATA=&CLASS_SRCH_WRK2_STRM$35$='
+        +term+
+        '&SSR_CLSRCH_WRK_SUBJECT_SRCH$0='
+        +subject+
+        '&SSR_CLSRCH_WRK_CATALOG_NBR$1='
+        +course+
+        '&SSR_CLSRCH_WRK_SSR_OPEN_ONLY$chk$5=N';
+};
+
+let urlProducerClassNumber = (ICStateNum, ICSID, term, classNum) => {
+    return 'https://hrsa.cunyfirst.cuny.edu/psc/cnyhcprd/GUEST/HRMS/c/COMMUNITY_ACCESS.CLASS_SEARCH.GBL?ICAJAX=1&ICNAVTYPEDROPDOWN=0&ICType=Panel&ICElementNum=0&ICStateNum='
+        +ICStateNum+
+        '&ICAction=CLASS_SRCH_WRK2_SSR_PB_CLASS_SRCH&ICXPos=0&ICYPos=142&ResponsetoDiffFrame=-1&TargetFrameName=None&FacetPath=None&ICFocus=&ICSaveWarningFilter=0&ICChanged=-1&ICAutoSave=0&ICResubmit=0&ICSID='
+        +ICSID+
+        '=&ICActionPrompt=false&ICBcDomData=undefined&ICFind=&ICAddCount=&ICAPPCLSDATA=&CLASS_SRCH_WRK2_STRM$35$='
+        +term+
+        '&SSR_CLSRCH_WRK_CLASS_NBR$10='
+        +classNum
+};
+
+let getAllSections = (inst, term, subject, callback) => {
     request.get(options, function(error, response, body){
         if(error){
             console.log('CUNYfirst is currently offline.');
@@ -73,7 +98,7 @@ let section = (inst, term, subject, callback) => {
         };
 
         request.get(submit_options, function(error, response, body){
-            submit_options['url'] = urlProducerClasses(++ICStateNum, ICSID, term, subject);
+            submit_options['url'] = urlProducerAllClasses(++ICStateNum, ICSID, term, subject);
             request.get(submit_options, function(error, response, body) {
                 let classes = {};
                 let $ = cheerio.load(body);
@@ -114,11 +139,144 @@ let section = (inst, term, subject, callback) => {
                     idTitle = `#win0divSSR_CLSRSLT_WRK_GROUPBOX2GP\\$${i}`;
                     idTable = `#ACE_\\$ICField48\\$${i}`;
                 }
+
                 callback(classes);
             })
         })
     })
-}
+};
+
+let getSpecificCourse = (inst, term, subject, course, callback) => {
+    request.get(options, function(error, response, body){
+        if(error){
+            console.log('CUNYfirst is currently offline.');
+        }
+
+        let ICValues = getICValues(body);
+
+        let ICStateNum = ICValues['ICStateNum'];
+        let ICSID = ICValues['ICSID'];
+
+        let submit_options = {
+            url: urlProducer(ICStateNum, ICSID, inst, term),
+            headers: {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36'},
+            jar: options.jar
+        };
+
+        request.get(submit_options, function(error, response, body){
+            submit_options['url'] = urlProducerSpecificCourse(++ICStateNum, ICSID, term, subject, course);
+            request.get(submit_options, function(error, response, body) {
+                let classes = {};
+                let $ = cheerio.load(body);
+
+                let i = 0;
+                let idTitle = `#win0divSSR_CLSRSLT_WRK_GROUPBOX2GP\\$${i}`;
+                let idTable = `#ACE_\\$ICField48\\$${i}`;
+
+                let section = 0;
+
+                while ($(idTitle).text() !== '') {
+                    classes[$(idTitle).text()] = {};
+
+                    for(let j = 0; j < ($(idTable).children()[0].children.length)/4; j++) {
+                        let classNumber = $(`#MTG_CLASS_NBR\\$${section}`).text();
+                        let className = $(`#MTG_CLASSNAME\\$${section}`).text();
+                        let time = $(`#MTG_DAYTIME\\$${section}`).text();
+                        let room = $(`#MTG_ROOM\\$${section}`).text();
+                        let instructor = $(`#MTG_INSTR\\$${section}`).text();
+                        let dates = $(`#MTG_TOPIC\\$${section}`).text();
+                        let status = $(`#win0divDERIVED_CLSRCH_SSR_STATUS_LONG\\$${section}`).children()[0].children[3].attribs.alt;
+                        let description = $(`#DERIVED_CLSRCH_DESCRLONG\\$${section}`).text();
+
+                        classes[$(idTitle).text()][classNumber] = {};
+                        classes[$(idTitle).text()][classNumber]['Class'] = classNumber;
+                        classes[$(idTitle).text()][classNumber]['Section'] = className;
+                        classes[$(idTitle).text()][classNumber]['Days & Time'] = time;
+                        classes[$(idTitle).text()][classNumber]['Room'] = room;
+                        classes[$(idTitle).text()][classNumber]['Instructor'] = instructor;
+                        classes[$(idTitle).text()][classNumber]['Dates'] = dates;
+                        classes[$(idTitle).text()][classNumber]['Status'] = status;
+                        classes[$(idTitle).text()][classNumber]['Description'] = description;
+
+                        section++;
+                    }
+
+                    i++;
+                    idTitle = `#win0divSSR_CLSRSLT_WRK_GROUPBOX2GP\\$${i}`;
+                    idTable = `#ACE_\\$ICField48\\$${i}`;
+                }
+
+                callback(classes);
+            })
+        })
+    })
+};
+
+let getClassByClassNumber = (inst, term, classNum, callback) => {
+    request.get(options, function(error, response, body){
+        if(error){
+            console.log('CUNYfirst is currently offline.');
+        }
+
+        let ICValues = getICValues(body);
+
+        let ICStateNum = ICValues['ICStateNum'];
+        let ICSID = ICValues['ICSID'];
+
+        let submit_options = {
+            url: urlProducer(ICStateNum, ICSID, inst, term),
+            headers: {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36'},
+            jar: options.jar
+        };
+
+        request.get(submit_options, function(error, response, body){
+            submit_options['url'] = urlProducerClassNumber(++ICStateNum, ICSID, term, classNum);
+            request.get(submit_options, function(error, response, body) {
+                let classes = {};
+                let $ = cheerio.load(body);
+
+                let i = 0;
+                let idTitle = `#win0divSSR_CLSRSLT_WRK_GROUPBOX2GP\\$${i}`;
+                let idTable = `#ACE_\\$ICField48\\$${i}`;
+
+                let section = 0;
+
+                while ($(idTitle).text() !== '') {
+                    classes[$(idTitle).text()] = {};
+
+                    for(let j = 0; j < ($(idTable).children()[0].children.length)/4; j++) {
+                        let classNumber = $(`#MTG_CLASS_NBR\\$${section}`).text();
+                        let className = $(`#MTG_CLASSNAME\\$${section}`).text();
+                        let time = $(`#MTG_DAYTIME\\$${section}`).text();
+                        let room = $(`#MTG_ROOM\\$${section}`).text();
+                        let instructor = $(`#MTG_INSTR\\$${section}`).text();
+                        let dates = $(`#MTG_TOPIC\\$${section}`).text();
+                        let status = $(`#win0divDERIVED_CLSRCH_SSR_STATUS_LONG\\$${section}`).children()[0].children[3].attribs.alt;
+                        let description = $(`#DERIVED_CLSRCH_DESCRLONG\\$${section}`).text();
+
+                        classes[$(idTitle).text()][classNumber] = {};
+                        classes[$(idTitle).text()][classNumber]['Class'] = classNumber;
+                        classes[$(idTitle).text()][classNumber]['Section'] = className;
+                        classes[$(idTitle).text()][classNumber]['Days & Time'] = time;
+                        classes[$(idTitle).text()][classNumber]['Room'] = room;
+                        classes[$(idTitle).text()][classNumber]['Instructor'] = instructor;
+                        classes[$(idTitle).text()][classNumber]['Dates'] = dates;
+                        classes[$(idTitle).text()][classNumber]['Status'] = status;
+                        classes[$(idTitle).text()][classNumber]['Description'] = description;
+
+                        section++;
+                    }
+
+                    i++;
+                    idTitle = `#win0divSSR_CLSRSLT_WRK_GROUPBOX2GP\\$${i}`;
+                    idTable = `#ACE_\\$ICField48\\$${i}`;
+                }
+
+                callback(classes);
+            })
+        })
+    })
+};
 
 let subject = (inst, term, callback) => {
     request.get(options, function(error, response, body){
@@ -184,7 +342,7 @@ let start = new Date().getTime();
 // institutions(function(){
 //     term('QNS01', function(){
 //         subject('QNS01', '1182', function(){
-//             section('QNS01', '1182', 'CSCI', function(r){
+//             getAllSections('QNS01', '1182', 'CSCI', function(r){
 //                 console.log(JSON.stringify(r, undefined, 2));
 //                 let end = new Date().getTime();
 //                 let time = end - start;
@@ -194,7 +352,21 @@ let start = new Date().getTime();
 //     });
 // });
 
-section('QNS01', '1182', 'CSCI', function(r){
+// getAllSections('QNS01', '1182', 'CSCI', function(r){
+//     console.log(JSON.stringify(r, undefined, 2));
+//     let end = new Date().getTime();
+//     let time = end - start;
+//     console.log('Execution time: ' + time);
+// });
+
+// getSpecificCourse('QNS01', '1182', 'PHYS', '103', function (r) {
+//     console.log(JSON.stringify(r, undefined, 2));
+//     let end = new Date().getTime();
+//     let time = end - start;
+//     console.log('Execution time: ' + time);
+// });
+
+getClassByClassNumber('QNS01', '1182', '22453', function(r){
     console.log(JSON.stringify(r, undefined, 2));
     let end = new Date().getTime();
     let time = end - start;
