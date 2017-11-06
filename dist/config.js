edce4d7d2134add95a877465f32e20216a8801df
Quazi <q.hossain@ymail.com>
2017-11-05 21:06:57 -0500

    updated worker.js

--- dist/config.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var config = {
-    cunyfirst_url: 'https://hrsa.cunyfirst.cuny.edu/psc/cnyhcprd/GUEST/HRMS/c/COMMUNITY_ACCESS.CLASS_SEARCH.GBL'
-};
-
-module.exports.config = config;
-//# sourceMappingURL=config.js.map
