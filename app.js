f1232787bf1a0becd574d72a72ccd8363ca50f52
Quazi <q.hossain@ymail.com>
2017-11-07 15:01:52 -0500

    created post for adding to database

--- app.js
+++ app.js
@@ -9,13 +9,27 @@ app.set('view engine', 'hbs');
 app.use(express.static(__dirname + 'public'));
 
 app.get('/', (req, res) => {
-   // res.render('index.hbs');
-    db.query('SELECT * FROM classes', [], (err, res) => {
-        if (err){
-            return console.error('Error fetching client from pool', err);
+   res.render('index.hbs');
+
+    // db.query('SELECT * FROM classes', [], (error, response) => {
+    //     if (error){
+    //         return console.error('Error fetching', error);
+    //     }
+    //     res.send(response.rows);
+    // })
+});
+
+app.post('/add', (req, res) => {
+    // res.render('index.hbs');
+
+    db.query("INSERT INTO classes (phone, institution, term, subject, class_num) VALUES ($1, $2, $3, $4, $5)",
+        [req.phone, req.institution, req.term, req.subject, req.class_num], (error, response) => {
+        console.log(req.phone);
+        if (error){
+            return console.error('Error fetching client', error);
         }
-        res.send(res.rows[0]);
-    })
+    });
+    res.redirect('/');
 });
 
 app.listen(3000, function(){
--- db/index.js
+++ db/index.js
@@ -1,13 +1,13 @@
 //not in here we will loop through the table and text a person if class is open and if not continue onto the next person
 //inserting will be done in another page or can it be done here?
 require('dotenv').config();
-// const { Client } = require('pg');
+const { Client } = require('pg');
 
 const database_url = process.env.DATABASE_url;
 
-// const client = new Client(database_url);
+const client = new Client(database_url);
 
-// client.connect();
+client.connect();
 
 // client.query("INSERT INTO classes(phone, institution, term, subject, class_num) VALUES ('3475276604@smtext.com', 'QNS01', '1182', 'CSCI', 1927)", (err, res) => {
 //     console.log(err, res);
@@ -19,12 +19,8 @@ const database_url = process.env.DATABASE_url;
 //     client.end()
 // });
 
-const { Pool } = require('pg');
-
-const pool = new Pool(database_url);
-
 module.exports = {
     query: (text, params, callback) => {
-        return pool.query(text, params, callback)
+        return client.query(text, params, callback)
     }
 };
