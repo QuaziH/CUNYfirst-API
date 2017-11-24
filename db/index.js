5400ff2621d4f2b088747ec5cb49ca377d34e52b
Quazi <q.hossain@ymail.com>
2017-11-23 19:48:03 -0500

    removed comments

--- db/index.js
+++ db/index.js
@@ -10,15 +10,6 @@ const pool = new Pool({
     port: process.env.PORT,
 });
 
-// pool.query("INSERT INTO classes(institution, term, subject, topic, class_num, phone, carrier) VALUES ('QNS01', '1182', 'CSCI', 'CSCI 331 ', 62280, '7188696520', '@tmomail.net')");
-// pool.query("INSERT INTO classes(institution, term, subject, topic, class_num, phone, carrier) VALUES ('QNS01', '1182', 'CSCI', 'CSCI 331 ', 22410, '3475276604', '@smtext.com')");
-// pool.query("INSERT INTO classes(institution, term, subject, topic, class_num, phone, carrier) VALUES ('QNS01', '1182', 'CSCI', 'CSCI 370 ', 22455, '3475276604', '@smtext.com')");
-// pool.query("INSERT INTO classes(institution, term, subject, topic, class_num, phone, carrier) VALUES ('QNS01', '1182', 'CSCI', 'CSCI 311 ', 22411, '3475276604', '@smtext.com')");
-// pool.query("INSERT INTO classes(institution, term, subject, topic, class_num, phone, carrier) VALUES ('QNS01', '1182', 'PHYS', 'PHYS 227 ', 1927, '3475276604', '@smtext.com')");
-// pool.query("INSERT INTO classes(institution, term, subject, topic, class_num, phone, carrier) VALUES ('QNS01', '1182', 'MATH', 'MATH 241 ', 22925, '3475276604', '@smtext.com')");
-// pool.query("INSERT INTO classes(institution, term, subject, topic, class_num, phone, carrier) VALUES ('QNS01', '1182', 'MATH', 'MATH 241 ', 22919, '3475276604', '@smtext.com')");
-// pool.query("INSERT INTO classes(institution, term, subject, topic, class_num, phone, carrier) VALUES ('QNS01', '1182', 'CSCI', 'CSCI 370 ', 22456, '3475276604', '@smtext.com')");
-
 // pool.query("SELECT * FROM classes");
 
 // pool.query("DELETE FROM classes");
@@ -28,17 +19,6 @@ module.exports = {
         return pool.query(text, params, callback)
     }
 };
-
-//Moustafa
-// pool.query("INSERT INTO classes(institution, term, subject, topic, class_num, phone, carrier) VALUES ('QNS01', '1182', 'CSCI', 'CSCI 343 ', 22422, '9175174711', '@tmomail.net')");
-// pool.query("INSERT INTO classes(institution, term, subject, topic, class_num, phone, carrier) VALUES ('QNS01', '1182', 'CSCI', 'CSCI 343 ', 22425, '9175174711', '@tmomail.net')");
-// pool.query("INSERT INTO classes(institution, term, subject, topic, class_num, phone, carrier) VALUES ('QNS01', '1182', 'CSCI', 'CSCI 343 ', 22815, '9175174711', '@tmomail.net')");
-// pool.query("INSERT INTO classes(institution, term, subject, topic, class_num, phone, carrier) VALUES ('QNS01', '1182', 'PHYS', 'PHYS 103 ', 1895, '9175174711', '@tmomail.net')");
-// pool.query("INSERT INTO classes(institution, term, subject, topic, class_num, phone, carrier) VALUES ('QNS01', '1182', 'PHYS', 'PHYS 103 ', 1896, '9175174711', '@tmomail.net')");
-// pool.query("INSERT INTO classes(institution, term, subject, topic, class_num, phone, carrier) VALUES ('QNS01', '1182', 'PHYS', 'PHYS 103 ', 1897, '9175174711', '@tmomail.net')");
-// pool.query("INSERT INTO classes(institution, term, subject, topic, class_num, phone, carrier) VALUES ('QNS01', '1182', 'MATH', 'MATH 231 ', 22194, '9175174711', '@tmomail.net')");
-// pool.query("INSERT INTO classes(institution, term, subject, topic, class_num, phone, carrier) VALUES ('QNS01', '1182', 'MATH', 'MATH 231 ', 22915, '9175174711', '@tmomail.net')");
-
 
 
 
